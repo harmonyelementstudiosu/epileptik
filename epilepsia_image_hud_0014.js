e9f31f8b6ab2d74cbf017591734f0c0a5abea95d
harmonyelementstudiosu <harmony@element-studio.su>
2025-11-23 01:05:16 +0300

    Add files via upload

--- epilepsia_image_hud_0014.js
+++ epilepsia_image_hud_0014.js
@@ -388,7 +388,7 @@ body #app .hud-radmir-radar__map {
   box-shadow: none;
 }
 #app .hud-radmir-radar { 
-  right: 146.2vh; /* Смещение вправо */
+  right: 142.2vh; /* Смещение вправо */
   left: auto; /* Отмена предыдущего смещения влево */
 }
       body .OLD-RADMIR-logo__bonus {
