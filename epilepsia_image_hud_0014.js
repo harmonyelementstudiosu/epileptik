b0ad7fc4208de09359d43170c95ced779d33b949
harmonyelementstudiosu <harmony@element-studio.su>
2025-11-23 14:16:07 +0300

    Add files via upload

--- epilepsia_image_hud_0014.js
+++ epilepsia_image_hud_0014.js
@@ -327,7 +327,7 @@ body #app .hud-radmir-info {
       }
 .Old-Fixed-Wanted {
    position: relative;
-   transform: translate(-155.2vh, 17.4vh); /* X, Y */
+   transform: translate(-155.2vh, 35.4vh); /* X, Y */
 }
 
 .Old-Fixed-Wanted_back {
